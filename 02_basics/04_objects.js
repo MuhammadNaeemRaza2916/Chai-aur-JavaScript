1cc04894ef4f6973ba5da2f716a2036ec9d60c38
Muhammad Naeem Raza <110351924+MuhammadNaeemRaza2916@users.noreply.github.com>
2023-11-02 06:57:34 +0000

    object part 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,11 +1,11 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object() // constructor object
+const tinderUser = {} // literals Object
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+// console.log(tinderUser); //Output => { id: '123abc', name: 'Sammy', isLoggedIn: false }
 
 const regularUser = {
     email: "some@gmail.com",
@@ -17,17 +17,21 @@ const regularUser = {
     }
 }
 
-// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname); // Output => hitesh
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = { obj1, obj2 } // Output => { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
 
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // Output => { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+// Object.assign(obj1, obj2, obj4) // Output => { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+// NOTE: above both object results are same but, in the first one all objects are asign in {} (spread), and then we hold them to new object obj3, where you also can see that obj3 and {} has same values, But in the second object obj2 and obj3 are assign (spread) in to the obj1  
+
+// const obj3 = {...obj1, ...obj2, ...obj4} // Output => { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+// console.log(obj1);
 
 
 const users = [
@@ -44,15 +48,15 @@ const users = [
         email: "h@gmail.com"
     },
 ]
+users[0].email
 
-users[1].email
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser)); // Output => [ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(tinderUser)); // Output => [ '123abc', 'Sammy', false ]
+// console.log(Object.entries(tinderUser)); // Output => [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); // true/false
 
 
 const course = {
@@ -63,10 +67,10 @@ const course = {
 
 // course.courseInstructor
 
-const {courseInstructor: instructor} = course
+// const {courseInstructor: instructor} = course
 
 // console.log(courseInstructor);
-console.log(instructor);
+// console.log(instructor);
 
 // {
 //     "name": "hitesh",
