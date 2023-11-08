561f0346311675adaea3f7af344424a430791aaf
Muhammad Naeem Raza <110351924+MuhammadNaeemRaza2916@users.noreply.github.com>
2023-11-08 11:28:25 +0000

    arrow function completed

--- 03_basics/03_arrowFunction.js
+++ 03_basics/03_arrowFunction.js
@@ -30,36 +30,33 @@ const user = {
 //   welcomeMessage: [Function: welcomeMessage]
 // }
 
-// if you check the context hold by this in global scope, it hold an emply object {} only in node,  
+// if you check the context hold by this in global scope, it hold an emply object {} only in node, and in browser console it holds hole window object of html file.
+
 // console.log(this);
 
+// this key word is just just for object context
 
+// 1st way to create functions
+function chai(){
+    let username = "hitesh"
+    console.log(this.username);
+}
 
+chai() // => Output : undefined
 
-
-
-
-
-
-
-
-
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
-
-// chai()
+// 2nd way to create functions
 
 // const chai = function () {
 //     let username = "hitesh"
 //     console.log(this.username);
 // }
 
-const chai = () => {
-  let username = "hitesh";
-  console.log(this);
-};
+// 3rd way to create functions
+
+// const chai = () => {
+//   let username = "hitesh";
+//   console.log(this);
+// };
 
 // chai()
 
@@ -67,10 +64,16 @@ const chai = () => {
 //     return num1 + num2
 // }
 
+// NOTE: if your code will be in only one line never use {}, for short syntax, and if you use {}, then you have to write return keyword
+
 // const addTwo = (num1, num2) =>  num1 + num2
+
+// if your code is short so you can also use '()' insteed of '{ return }'
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
+
+// in the bellow example you see '({})' because '{}' is for the object block
 const addTwo = (num1, num2) => ({ username: "hitesh" });
 
 // console.log(addTwo(3, 4))
