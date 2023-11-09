9c6d6bae2e8e887b84fc6be683292f6cb611b37f
Muhammad Naeem Raza <110351924+MuhammadNaeemRaza2916@users.noreply.github.com>
2023-11-09 04:14:30 +0000

    for loops

---
+++ 05_iterations.js/one.js
@@ -0,0 +1,52 @@
+// 'for' repeat any work for multiple times based on some conditions 
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        //console.log("5 is best number");
+    }
+    //console.log(element);
+    
+}
+
+// console.log(element);
+// nested for loop
+
+for (let i = 1; i <= 10; i++) {
+    //console.log(`Outer loop value: ${i}`);
+   for (let j = 1; j <= 10; j++) {
+    //console.log(`Inner loop value ${j} and inner loop ${i}`);
+    //console.log(i + '*' + j + ' = ' + i*j );
+   }
+    
+}
+let myArray = ["flash", "batman", "superman"]
+//console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+    
+}
+
+
+// break and continue
+// 'break' when the condition meets, break keyword will stop all operation of its block and escape from it.
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//    console.log(`Value of i is ${index}`);
+    
+// }
+// 'continue' when conditions meets, then only that condition will escape, other conditions will be continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+   console.log(`Value of i is ${index}`);
+    
+}
